aa6ad39cec377e59c2f06d13295ce514b75b6469
rwlang <ryanw.langridge@gmail.com>
2020-06-19 13:35:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -106,6 +106,7 @@ this.options.events['click button#fullscreen'] = function openFullscreen() {
   } else if (elem.msRequestFullscreen) { /* IE/Edge */
     elem.msRequestFullscreen();
   }
+  console.log(util.Random.uuid4());
 
 /* Continue to next screen */
 this.end()
